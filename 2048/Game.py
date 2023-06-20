0f4b0742159bdcc539e9f5559ea680f4f51109a6
Romulo <romulotg12@gmail.com>
2023-06-20 01:38:34 -0300

    project structure

--- 2048/Game.py
+++ 2048/Game.py
@@ -1,23 +1,40 @@
 import random
+from graphics import *
+import consts as C
 
 ##############################################
 #                                            #
 #                                            #
 #             LOGIC - 2048 GAME              #
 #                                            #
-#                                            # 
-##############################################
+#                                            #
+############################################## 
 
 class Game:
-    def __init__(self, player):
+    def __init__(self, win):
         self.score = 0
         self.board = [[None,None,None,None],
                       [None,None,None,None],
                       [None,None,None,None],
                       [None,None,None,None]]
+        
+        self.win = win
+
+        self.drawScore()
+        self.gameLoop()
 
     def updateScore(self, points):
         self.score += points
+
+        self.currentScore.undraw()
+        self.drawScore()
+
+    def drawScore(self):
+        self.currentScore = Text(Point(265, 80), self.score)
+        self.currentScore.setStyle("bold")
+        self.currentScore.setFill(C.GAME_MAIN_COLOR)
+        self.currentScore.setSize(20)
+        self.currentScore.draw(self.win)
 
     def generateNewPiece(self):
         done = False
@@ -51,13 +68,44 @@ class Game:
                 for column in row:
                     if column - 1 == None:
                         self.board[column - 1] = column 
+
+    def drawPieces(self):
+        for row in range(0, 4):
+            for column in range(0, 4):
+                if self.board[row][column] != None:
+                    num = self.board[row][column]
+                    self.drawPiece(row,column,num)
+
+                
+
+    def drawPiece(self,row,column,num):
+        VALOR_X = 7 + (column * 105)
+        VALOR_Y = 152 + (row * 105)
+
+        piece = Rectangle(Point(VALOR_X, VALOR_Y), Point(VALOR_X + 96, VALOR_Y + 96))
+        piece.setFill(C.CELL_COLOR_DICT[num])
+        piece.setWidth(0)
+        piece.draw(self.win)
     
     def gameLoop(self):
-        playerMovement = ""
 
         done = False
         while not done:
-        
-            self.movePieces(playerMovement)
+
+            moveKey = self.win.getKey()
+
+            self.drawPieces()
+
+            if moveKey == "Right" or moveKey == "D" or moveKey == "d":
+                pass
+
+            if moveKey == "Up" or moveKey == "W" or moveKey == "w":
+                pass
+
+            if moveKey == "Down" or moveKey == "S" or moveKey == "s":
+                pass
+
+            if moveKey == "Left" or moveKey == "A" or moveKey == "a":
+                pass
 
     
--- 2048/visual.py
+++ 2048/visual.py
@@ -1,31 +1,56 @@
 from graphics import *
-import consts as CONS
+import consts as C
 
-def squares():
-    SQUARES = []
-    SQUARES.append(Rectangle(Point(5, 150), Point(105, 250)))
-    SQUARES.append(Rectangle(Point(110, 150), Point(210, 250)))
-    SQUARES.append(Rectangle(Point(215, 150), Point(315, 250)))
-    SQUARES.append(Rectangle(Point(320, 150), Point(420, 250)))
+def staticVisual():
+    GAME_VISUAL = []
 
-    SQUARES.append(Rectangle(Point(5, 255), Point(105, 355)))
-    SQUARES.append(Rectangle(Point(110, 255), Point(210, 355)))
-    SQUARES.append(Rectangle(Point(215, 255), Point(315, 355)))
-    SQUARES.append(Rectangle(Point(320, 255), Point(420, 355)))
+    squareWindow = Rectangle(Point(0, 145), Point(425, 570))
+    squareWindow.setFill(C.CELL_BORDER_COLOR)
+    squareWindow.setWidth(0)
+    GAME_VISUAL.append(squareWindow)
 
-    SQUARES.append(Rectangle(Point(5, 360), Point(105, 460)))
-    SQUARES.append(Rectangle(Point(110, 360), Point(210, 460)))
-    SQUARES.append(Rectangle(Point(215, 360), Point(315, 460)))
-    SQUARES.append(Rectangle(Point(320, 360), Point(420, 460)))
+    #STATIC SQUARES
+    GAME_VISUAL.append(Rectangle(Point(5, 150), Point(105, 250)))
+    GAME_VISUAL.append(Rectangle(Point(110, 150), Point(210, 250)))
+    GAME_VISUAL.append(Rectangle(Point(215, 150), Point(315, 250)))
+    GAME_VISUAL.append(Rectangle(Point(320, 150), Point(420, 250)))
 
-    SQUARES.append(Rectangle(Point(5, 465), Point(105, 565)))
-    SQUARES.append(Rectangle(Point(110, 465), Point(210, 565)))
-    SQUARES.append(Rectangle(Point(215, 465), Point(315, 565)))
-    SQUARES.append(Rectangle(Point(320, 465), Point(420, 565)))
+    GAME_VISUAL.append(Rectangle(Point(5, 255), Point(105, 355)))
+    GAME_VISUAL.append(Rectangle(Point(110, 255), Point(210, 355)))
+    GAME_VISUAL.append(Rectangle(Point(215, 255), Point(315, 355)))
+    GAME_VISUAL.append(Rectangle(Point(320, 255), Point(420, 355)))
 
-    for item in SQUARES:
+    GAME_VISUAL.append(Rectangle(Point(5, 360), Point(105, 460)))
+    GAME_VISUAL.append(Rectangle(Point(110, 360), Point(210, 460)))
+    GAME_VISUAL.append(Rectangle(Point(215, 360), Point(315, 460)))
+    GAME_VISUAL.append(Rectangle(Point(320, 360), Point(420, 460)))
+
+    GAME_VISUAL.append(Rectangle(Point(5, 465), Point(105, 565)))
+    GAME_VISUAL.append(Rectangle(Point(110, 465), Point(210, 565)))
+    GAME_VISUAL.append(Rectangle(Point(215, 465), Point(315, 565)))
+    GAME_VISUAL.append(Rectangle(Point(320, 465), Point(420, 565)))
+
+    for item in GAME_VISUAL:
         item.setWidth(5)
-        item.setOutline(CONS.CELL_BORDER_COLOR)
-        item.setFill(CONS.BACKGROUND_COLOR_CELL_EMPTY)
+        item.setOutline(C.CELL_BORDER_COLOR)
+        item.setFill(C.BACKGROUND_COLOR_CELL_EMPTY)
 
-    return SQUARES
+    gameName = Text(Point(55, 72.5), "2048")
+    gameName.setStyle("bold")
+    gameName.setFill(C.GAME_MAIN_COLOR)
+    gameName.setSize(25)
+    GAME_VISUAL.append(gameName)
+
+    squareScore = Rectangle(Point(160, 40), Point(370, 100))
+    squareScore.setWidth(2)
+    squareScore.setOutline(C.GAME_MAIN_COLOR)
+    GAME_VISUAL.append(squareScore)
+
+    scoreWord = Text(Point(265, 50), "SCORE")
+    scoreWord.setStyle("bold")
+    scoreWord.setFill(C.GAME_MAIN_COLOR)
+    scoreWord.setSize(12)
+    GAME_VISUAL.append(scoreWord)
+
+
+    return GAME_VISUAL
